9ac85a61e34d57dcc04ae68f7cce2beefda9d168
Jonathan Utsey <JUtsey09@users.noreply.github.com>
2017-07-23 17:37:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$('.fade').slick({                
+  dots: true,                   //Settings for the slick carousel
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  accessibility: true,
+  autoplay: true,
+  autoplaySpeed: 3000
+});
